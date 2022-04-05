a27477270e6f2ea920d27291d8faaa8a0a2dbd42
Polina Suboch <palina.subach@gmail.com>
2022-04-05 14:36:49 +0400

    Add authentification functionality

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-A test Blog project written on Flask and following the tutorial from Miguel Grinberg.
-
+###About this project
+This project presents a Blog written on Python using Flask web framework. Implemented following the tutorial of Miguel Grinberg:\
 https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+
+###Technologies used
+Flask Web Framework and extensions:
+* Flask-SQLAlchemy
+* Flask-Migrate
+* Flask-Login
+* Flask-WTF
+
+Database: SQLite
--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+from app.models import User
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=32)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=32)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=32)])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign in')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=32)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=32)])
+    password2 = PasswordField('Repeat Password',
+                              validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('This username already exists.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('This email address already exists.')
