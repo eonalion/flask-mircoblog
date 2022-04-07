0ccc5bdf0841a84a56e101513db512e0baf6e7e0
Polina Suboch <palina.subach@gmail.com>
2022-04-07 14:05:28 +0400

    Implement model for following users

--- app/models.py
+++ app/models.py
@@ -13,6 +13,12 @@ def load_user(id):
     return User.query.get(int(id))
 
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('subscription_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -21,7 +27,11 @@ class User(UserMixin, db.Model):
     image = db.Column(db.String(128))
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', cascade="all, delete", lazy='dynamic')
+    subscriptions = db.relationship('User', secondary=followers,
+                                    primaryjoin=followers.c.follower_id == id,
+                                    secondaryjoin=followers.c.subscription_id == id,
+                                    backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -32,6 +42,22 @@ class User(UserMixin, db.Model):
     def default_avatar(self):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon'
+
+    def follow(self, user):
+        if user not in self.subscriptions:
+            self.subscriptions.append(user)
+
+    def unfollow(self, user):
+        if user in self.subscriptions:
+            self.subscriptions.remove(user)
+
+    def subscription_posts(self):
+        subscription_posts = Post.query.join(
+            followers, (followers.c.subscription_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+        own_posts = Post.query.filter_by(user_id=self.id)
+
+        return subscription_posts.union(own_posts).order_by(Post.timestamp.desc())
 
     def __repr__(self):
         return f'<User {self.username}>'
