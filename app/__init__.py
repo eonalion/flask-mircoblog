a096057616d4edf0d4028d3943d72173ef8de14a
Polina Suboch <palina.subach@gmail.com>
2022-04-10 21:21:56 +0400

    Refactor amd extract error blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,13 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models, errors
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
+
+from app import routes, models
